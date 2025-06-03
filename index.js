1aad78b32058827e23d221f1c43586e6cc9ab69c
chantewyngaardt <chantexwyngaardt@icloud.com>
2025-06-03 12:46:54 +0200

    test forgot pw removed

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ import userRouter from './routes/userRouter.js';
 import { transporter } from './config/emailTransporter.js';
 
 config();
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "[HIDDEN]" : "NOT FOUND");
+const app = express();
+
+// console.log("EMAIL_USER:", process.env.EMAIL_USER);
+// console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "[HIDDEN]" : "NOT FOUND");
 
 // Middleware to parse JSON
 app.use(cors());
@@ -14,20 +16,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Test email route
-app.get("/test-email", async (req, res) => {
-  try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: "your-test-email@example.com",
-      subject: "Test Email from Node.js",
-      text: "This is a test email using Nodemailer.",
-    });
-    res.send("✅ Email sent successfully!");
-  } catch (err) {
-    console.error("❌ Email sending failed:", err);
-    res.status(500).send("Email failed to send.");
-  }
-});
+// app.get("/test-email", async (req, res) => {
+//   try {
+//     await transporter.sendMail({
+//       from: process.env.EMAIL_USER,
+//       to: "indiphilemcengwa.29@gmail.com",
+//       subject: "Test Email from Node.js",
+//       text: "This is a test email using Nodemailer.",
+//     });
+//     res.send("✅ Email sent successfully!");
+//   } catch (err) {
+//     console.error("❌ Email sending failed:", err);
+//     res.status(500).send("Email failed to send.");
+//   }
+// });
 
 // Routes
 app.use('/', userRouter);
